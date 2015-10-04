312b76ae2dd00f3d46d0bf9c32291790a3cf59a4
Avidan Borisov <Avidanborisov@gmail.com>
2015-10-05 00:26:45 +0300

    Clean screen

--- kernel/kernel.cpp
+++ kernel/kernel.cpp
@@ -1,5 +1,13 @@
+#include <stdint.h>
 #include "kernel.hpp"
 
 void Kernel::main() {
-    *(unsigned char *)0xB8CD0 = 'A'; // print 'A' on the screen somewhere visible
+    using Monitor = uint16_t[25][80];
+    auto& monitor = *(Monitor *)0xB8000;
+
+    for (auto& row : monitor)
+        for (auto& cell : row)
+            cell = ' ';
+
+    monitor[0][0] = 'A' | 0xf00;
 }
